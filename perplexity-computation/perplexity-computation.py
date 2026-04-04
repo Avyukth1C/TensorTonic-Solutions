9a4b9a20e6759ce3e5cb7690b72ba33804245746
Avyukth Potnuru <86914215+Avyukth1C@users.noreply.github.com>
2026-04-05 00:28:18 +0530

    Add solution for perplexity-computation

---
+++ perplexity-computation/perplexity-computation.py
@@ -0,0 +1,12 @@
+def perplexity(prob_distributions, actual_tokens):
+    """
+    Compute the perplexity of a token sequence given predicted distributions.
+    """
+    summ = 0
+    for i in range(len(actual_tokens)):
+        p = prob_distributions[i][actual_tokens[i]]
+        summ += math.log(p)
+
+    perp = math.exp((-1/len(actual_tokens))*summ)
+
+    return perp
